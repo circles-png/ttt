ee1e7ffc74b9379df852e2a67fe64cfb8af821a2
Matthew Li <matthew.li10@education.nsw.gov.au>
2024-11-18 20:14:19 +1100

    Refactor game initialization and remove unused random functionality; add animation data

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
-#![feature(random, abi_avr_interrupt, array_chunks)]
+#![feature(abi_avr_interrupt, array_chunks)]
 #![no_std]
 #![no_main]
 
 use core::{
     array::from_fn,
     convert::{identity, Into},
-    random::{Random, RandomSource},
 };
 
 use arduino_hal::{
-    adc::{AdcOps, AdcSettings},
     hal::port::Dynamic,
-    pac::{adc::admux::MUX_A, PORTC},
     pins,
     port::mode::{Input, OpenDrain, Output, PullUp},
     Peripherals,
@@ -21,63 +18,6 @@ use itertools::Itertools;
 use millis::{init, millis};
 use panic_halt as _;
 mod millis;
-
-fn random<T: Random>(source: &mut (impl RandomSource + ?Sized)) -> T {
-    T::random(source)
-}
-
-struct NanoRandom([u64; 2]);
-
-impl NanoRandom {
-    fn new<H, A>(adc: &mut A) -> Self
-    where
-        A: AdcOps<H, Settings = AdcSettings, Channel = MUX_A>,
-    {
-        let mut last = None;
-
-        Self(from_fn(|_| {
-            u64::from_be_bytes(from_fn(|_| {
-                let byte = {
-                    adc.raw_enable_channel(MUX_A::ADC5);
-                    unsafe {
-                        (*PORTC::PTR).ddrc.write(|w| w.pc5().clear_bit());
-                        (*PORTC::PTR).portc.write(|w| w.pc5().clear_bit());
-                    };
-                    adc.raw_set_channel(MUX_A::ADC5);
-                    adc.raw_start_conversion();
-                    while adc.raw_is_converting() {}
-                    adc.raw_read_adc().to_be_bytes()[1]
-                };
-                let byte = last.map_or(byte, |last| byte ^ last);
-                last = Some(byte);
-                byte
-            }))
-        }))
-    }
-
-    fn next(&mut self) -> u64 {
-        let first = self.0[0];
-        let mut second = self.0[1];
-        let result = first.wrapping_add(second);
-
-        second ^= first;
-        self.0[0] = first.rotate_left(55) ^ second ^ (second << 14);
-        self.0[1] = second.rotate_left(36);
-
-        result
-    }
-}
-
-impl RandomSource for NanoRandom {
-    fn fill_bytes(&mut self, bytes: &mut [u8]) {
-        for byte in bytes {
-            #[allow(clippy::cast_possible_truncation)]
-            {
-                *byte = self.next() as u8;
-            }
-        }
-    }
-}
 
 struct Game {
     board: Board,
@@ -89,20 +29,24 @@ struct Game {
     led_rows: [Pin<OpenDrain, Dynamic>; 3],
     button_columns: [Pin<Input<PullUp>, Dynamic>; 3],
     button_rows: [Pin<OpenDrain, Dynamic>; 3],
+    animation: [Board<DisplayPixel>; 12],
 }
 
 impl Game {
     fn new() -> Self {
-        let mut peripherals = Peripherals::take().unwrap();
+        let peripherals = Peripherals::take().unwrap();
         let pins = pins!(peripherals);
-        let mut nano_random = NanoRandom::new(&mut peripherals.ADC);
         init(&peripherals.TC0);
         macro_rules! p {
             ($mode:ident, $($pins:ident),+) => {
                 [$(pins.$pins.$mode().downgrade()),+]
             };
         }
-        let current_player = random(&mut nano_random);
+        let current_player = if pins.a5.into_pull_up_input().is_high() {
+            Player::Red
+        } else {
+            Player::Blue
+        };
         Self {
             board: [[None; 3]; 3],
             current_player,
@@ -113,62 +57,96 @@ impl Game {
             led_rows: p!(into_opendrain, d8, d9, d10),
             button_columns: p!(into_pull_up_input, d11, d12, d13),
             button_rows: p!(into_opendrain, a0, a1, a2),
+            animation: {
+                let mut iter = include_str!("animation")
+                    .trim()
+                    .split("\n\n")
+                    .map(|display| {
+                        let mut iter = display.lines().map(|line| {
+                            let mut iter = line.chars().map(|char| match char {
+                                'r' => DisplayPixel {
+                                    red: true,
+                                    blue: false,
+                                },
+                                'b' => DisplayPixel {
+                                    red: false,
+                                    blue: true,
+                                },
+                                '-' => DisplayPixel {
+                                    red: false,
+                                    blue: false,
+                                },
+                                _ => panic!(),
+                            });
+                            let result = from_fn(|_| iter.next().unwrap());
+                            assert!(iter.next().is_none());
+                            result
+                        });
+                        let result = from_fn(|_| iter.next().unwrap());
+                        assert!(iter.next().is_none());
+                        result
+                    });
+                let result = from_fn(|_| iter.next().unwrap());
+                assert!(iter.next().is_none());
+                result
+            },
         }
     }
 
     fn run(mut self) {
         loop {
-            match self.state {
-                GameState::PresentCurrentPlayer => {
-                    if millis() > 1000 {
-                        self.state = GameState::WaitForMove;
-                    }
-                }
-                GameState::WaitForMove => {
-                    if let Ok((x, y)) = self
-                        .get_buttons()
-                        .into_iter()
-                        .enumerate()
-                        .filter_map(|(y, row)| {
-                            row.into_iter()
-                                .enumerate()
-                                .filter_map(|(x, pressed)| pressed.then_some(x))
-                                .exactly_one()
-                                .ok()
-                                .map(|x| (x, y))
-                        })
-                        .exactly_one()
-                    {
-                        if self.board[y][x].is_none() {
-                            self.board[y][x] = Some(self.current_player);
-                            self.current_player.flip();
-                        };
-                    }
-                    self.display = self.board.map(|row| row.map(Into::into));
-                    if let Some(winner) = self.winner() {
-                        self.state = GameState::DisplayWinner(winner);
-                    }
-                    if self.board.as_flattened().iter().all(Option::is_some) {
-                        self.state = GameState::DisplayTie;
-                    }
-                }
-                GameState::DisplayWinner(winner) => {
-                    let mut pixels = from_fn::<_, { 3 * 3 }, _>(|index| {
-                        DisplayPixel::from(winner)
-                            .and(index % 2 == usize::from(millis() % 1000 < 500))
-                    })
-                    .into_iter();
-                    self.display = from_fn(|_| from_fn(|_| pixels.next().unwrap()));
-                }
-                GameState::DisplayTie => {
-                    let mut pixels = from_fn::<_, { 3 * 3 }, _>(|index| DisplayPixel {
-                        red: index % 2 == usize::from(millis() % 1000 < 500),
-                        blue: index % 2 != usize::from(millis() % 1000 < 500),
-                    })
-                    .into_iter();
-                    self.display = from_fn(|_| from_fn(|_| pixels.next().unwrap()));
-                }
-            }
+            self.display = self.animation[(millis() as usize / 100) % 12];
+            // match self.state {
+            //     GameState::PresentCurrentPlayer => {
+            //         if millis() > 1000 {
+            //             self.state = GameState::WaitForMove;
+            //         }
+            //     }
+            //     GameState::WaitForMove => {
+            //         if let Ok((x, y)) = self
+            //             .get_buttons()
+            //             .into_iter()
+            //             .enumerate()
+            //             .filter_map(|(y, row)| {
+            //                 row.into_iter()
+            //                     .enumerate()
+            //                     .filter_map(|(x, pressed)| pressed.then_some(x))
+            //                     .exactly_one()
+            //                     .ok()
+            //                     .map(|x| (x, y))
+            //             })
+            //             .exactly_one()
+            //         {
+            //             if self.board[y][x].is_none() {
+            //                 self.board[y][x] = Some(self.current_player);
+            //                 self.current_player.flip();
+            //             };
+            //         }
+            //         self.display = self.board.map(|row| row.map(Into::into));
+            //         if let Some(winner) = self.winner() {
+            //             self.state = GameState::DisplayWinner(winner);
+            //         }
+            //         if self.board.as_flattened().iter().all(Option::is_some) {
+            //             self.state = GameState::DisplayTie;
+            //         }
+            //     }
+            //     GameState::DisplayWinner(winner) => {
+            //         let mut pixels = from_fn::<_, { 3 * 3 }, _>(|index| {
+            //             DisplayPixel::from(winner)
+            //                 .and(index % 2 == usize::from(millis() % 1000 < 500))
+            //         })
+            //         .into_iter();
+            //         self.display = from_fn(|_| from_fn(|_| pixels.next().unwrap()));
+            //     }
+            //     GameState::DisplayTie => {
+            //         let mut pixels = from_fn::<_, { 3 * 3 }, _>(|index| DisplayPixel {
+            //             red: index % 2 == usize::from(millis() % 1000 < 500),
+            //             blue: index % 2 != usize::from(millis() % 1000 < 500),
+            //         })
+            //         .into_iter();
+            //         self.display = from_fn(|_| from_fn(|_| pixels.next().unwrap()));
+            //     }
+            // };
             self.show_display();
         }
     }
@@ -235,16 +213,6 @@ impl Game {
 enum Player {
     Red,
     Blue,
-}
-
-impl Random for Player {
-    fn random(source: &mut (impl RandomSource + ?Sized)) -> Self {
-        if random(source) {
-            Self::Red
-        } else {
-            Self::Blue
-        }
-    }
 }
 
 impl Player {
